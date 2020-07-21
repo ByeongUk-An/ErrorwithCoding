5293e5efa0bafe33f527590fc17b5a8d8dec2b44
ByeongUk-An <61609264+ByeongUk-An@users.noreply.github.com>
2020-07-21 17:12:35 +0900

    section3완료

--- project3/src/Components/Section.js
+++ project3/src/Components/Section.js
@@ -7,6 +7,20 @@ import title1 from "../img/Title1.png";
 import title2 from "../img/Title2.png";
 import title3 from "../img/Title3.png";
 import Brush from "../img/Brush.png";
+import plate from "../img/plate.png";
+import food1 from "../img/food1.png";
+import food2 from "../img/food2.png";
+import food3 from "../img/food3.png";
+import food4 from "../img/food4.png";
+import food5 from "../img/food5.png";
+import food6 from "../img/food6.png";
+import food1t from "../img/food1t.png";
+import food2t from "../img/food2t.png";
+import food3t from "../img/food3t.png";
+import food4t from "../img/food4t.png";
+import food5t from "../img/food5t.png";
+import food6t from "../img/food6t.png";
+import price from "../img/price.png";
 
 const SectionWrap = styled.div`
   background: #212121;
@@ -43,10 +57,9 @@ const Section1_1_para = styled.p`
   line-height: 26px;
 `;
 const Section1_2 = styled.div`
-  width: 50%;
+  width: 44%;
   height: 100%;
   background: url(${images4}) no-repeat;
-  margin-left: 104px;
 `;
 // Section2
 const Section2 = styled.section`
@@ -80,15 +93,49 @@ const Sectiion2_p = styled.p`
 const Section3 = styled.section`
   width: 100%;
   height: 1205px;
-  background: powderblue;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 const Section3Wrap = styled.div`
   width: 1080px;
-  height: 1030px;
+  height: 1035px;
   background-color: #45493b;
+`;
+const Section3Title = styled.div`
+  height: 210px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Section3Title_text = styled.p`
+  color: #eaeaea;
+  margin-top: 10px;
+  font-size: 42px;
+  text-transform: uppercase;
+`;
+const Section3Box = styled.div`
+  width: 100%;
+  height: 825px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Section3Boxitem = styled.div`
+  width: 25%;
+  height: 275px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Section3Boxitem_p = styled.p`
+  /* width: 180px; */
+  color: #eaeaea;
+  font-size: 16.67px;
+  line-height: 20px;
+  margin-top: 27px;
+  margin-bottom: 22px;
 `;
 class Section extends React.Component {
   render() {
@@ -152,7 +199,98 @@ class Section extends React.Component {
           </Section2_ul>
         </Section2>
         <Section3>
-          <Section3Wrap></Section3Wrap>
+          <Section3Wrap>
+            <Section3Title>
+              <img src={plate} />
+              <Section3Title_text>to your plate</Section3Title_text>
+            </Section3Title>
+            <Section3Box>
+              <Section3Boxitem>
+                <img src={food1} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food1t} />
+                <Section3Boxitem_p>
+                  Lorem ipsum dolor
+                  <br />
+                  sit amet,consectetur
+                  <br />
+                  adipisicing elit set do
+                </Section3Boxitem_p>
+                <img src={price} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food2} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food2t} />
+                <Section3Boxitem_p>
+                  Lorem ipsum dolor
+                  <br />
+                  sit amet,consectetur
+                  <br />
+                  adipisicing elit set do
+                </Section3Boxitem_p>
+                <img src={price} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food3t} />
+                <Section3Boxitem_p>
+                  Lorem ipsum dolor
+                  <br />
+                  sit amet,consectetur
+                  <br />
+                  adipisicing elit set do
+                </Section3Boxitem_p>
+                <img src={price} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food3} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food4t} />
+                <Section3Boxitem_p>
+                  Lorem ipsum dolor
+                  <br />
+                  sit amet,consectetur
+                  <br />
+                  adipisicing elit set do
+                </Section3Boxitem_p>
+                <img src={price} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food4} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food5} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food5t} />
+                <Section3Boxitem_p>
+                  Lorem ipsum dolor
+                  <br />
+                  sit amet,consectetur
+                  <br />
+                  adipisicing elit set do
+                </Section3Boxitem_p>
+                <img src={price} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food6} />
+              </Section3Boxitem>
+              <Section3Boxitem>
+                <img src={food6t} />
+                <Section3Boxitem_p>
+                  Lorem ipsum dolor
+                  <br />
+                  sit amet,consectetur
+                  <br />
+                  adipisicing elit set do
+                </Section3Boxitem_p>
+                <img src={price} />
+              </Section3Boxitem>
+            </Section3Box>
+          </Section3Wrap>
         </Section3>
       </SectionWrap>
     );
