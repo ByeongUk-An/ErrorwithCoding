5477c8d1d0d8500d62c3e6fa98eacad5392767f2
ByeongUk-An <quddnr005@naver.com>
2020-11-08 09:37:26 +0900

    redux 연습

--- redux-practice2/src/index.js
+++ redux-practice2/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import reducer from "./redux/reducer";
 
-const store = createStore(); //리듀서넣기
+const store = createStore(reducer); // reducer 넣어주기
 
 ReactDOM.render(
   <React.StrictMode>
--- redux-practice2/src/redux/action.js
+++ redux-practice2/src/redux/action.js
@@ -5,8 +5,15 @@ export const addToDo = (todostate) => {
   return {
     type: ADD_TODO,
     todo: {
-      task: todoState,
-      id: Math.floor(Math.random()*100000000),
+      task: todostate,
+      id: Math.floor(Math.random() * 10000000),
     },
+  };
+};
+
+export const removeToDo = (id) => {
+  return {
+    type: REMOVE_TODO,
+    id,
   };
 };
